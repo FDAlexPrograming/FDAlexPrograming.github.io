7b1bbfdac1526aee568b84545b843f20daa5de44
Alex Farias Dos Snatos <fariasdossantosalex@gmail.com>
2022-11-07 20:03:54 -0300

    fix 3

--- TP3/js/load-and-classes.js
+++ TP3/js/load-and-classes.js
@@ -344,7 +344,7 @@ function iniciarPagina() {
     const TIEMPO_DE_JUEGO = 300;
     const CANVAS_WIDTH = 725;
     const CANVAS_HEIGHT = 607;
-    const CANVAS_IMG_BACKGROUND = "./isset/Goku.png";
+    const CANVAS_IMG_BACKGROUND = "./isset/goku.png";
     const CANVAS_IMG_BOX = "./isset/nube.png";
 
     let imageFondo = new Image();
